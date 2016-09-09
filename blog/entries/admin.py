8b838ea716c67f704730dc6e1cfed20fae32de83
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-09-09 14:33:49 +0200

    change_blog.html

--- blog/entries/admin.py
+++ blog/entries/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.shortcuts import render, redirect
 from entries.models import Blog, Author, Entry
 from entries.forms import SelectBlogForm
+
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
@@ -18,11 +20,26 @@ class AuthorAdmin(admin.ModelAdmin):
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('id', 'blog', 'headline', 'number_comments', 'scoring')
     list_filter = ('blog',)
-    actions = ['reset_scoring']
+    actions = ['reset_scoring', 'change_blog']
 
     def reset_scoring(self, request, queryset):
         rows_updated = queryset.update(scoring=0)
         self.message_user(request, '{} entry score(s) reset.'.format(rows_updated))
 
     def change_blog(self, request, queryset):
-        pass
+        form = SelectBlogForm()
+        from pprint import pprint
+        print(request.POST)
+        pprint(request.POST)
+        print('apply' in request.POST)
+        selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        print(selected)
+        form = SelectBlogForm(
+            initial={
+                '_selected_action': selected
+                                    }
+        )
+
+        # return redirect(request.get_full_path())
+        context = {'form': form}
+        return render(request, 'entries/admin/change_blog.html', context)
